efd6fa8d54734ea49f23b9eebeca5f418312eb8c
Richard Snider <richardnsnider@gmail.com>
2022-05-10 14:01:51 -0700

    update run script

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 set -xe
 
+sudo docker pull ghcr.io/richardsnider/go-wasm:latest
+
 docker run \
 --publish 8080:80 \
 --detach \
 --tty \
 --interactive \
 --rm \
-wasm_go
+ghcr.io/richardsnider/go-wasm:latest
+
+curl localhost:8080
